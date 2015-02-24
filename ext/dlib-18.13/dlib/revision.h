5ae1f83144a8d06e3b06d0c1fc799bed3738b659
Kenta Murata <mrkn@cookpad.com>
2015-02-24 12:42:51 +0900

    Import dlib-18.13

---
+++ ext/dlib-18.13/dlib/revision.h
@@ -0,0 +1,7 @@
+#ifndef DLIB_REVISION_H
+// Version:  18.13
+// Date:     Tue Feb 3 18:53:09 EST 2015
+// Mercurial Revision ID:  67c3ad208aae
+#define DLIB_MAJOR_VERSION  18
+#define DLIB_MINOR_VERSION  13
+#endif
