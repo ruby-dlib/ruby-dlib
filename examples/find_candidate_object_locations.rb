d18528366e779faeeb1a332dbeb893a8f5522d13
Kenta Murata <mrkn@cookpad.com>
2015-02-26 12:35:44 +0900

    Add Image#draw_rectangle and Image#save_jpeg

--- examples/find_candidate_object_locations.rb
+++ examples/find_candidate_object_locations.rb
@@ -7,5 +7,9 @@ rects = Dlib.find_candidate_object_locations(img, min_size: 500)
 
 puts "number of rectangles found #{rects.length}"
 rects.each.with_index do |rect, index|
-  puts "Detection #{index}: Left: #{rect.left} Top: #{rect.top} Right: #{rect.right} Bottom: #{rect.bottom}"
+  puts "Detection #{index}: Left: #{rect.left} Top: #{rect.top} Right: #{rect.right} Bottom: #{rect.bottom} Area: #{rect.area}"
+  img.draw_rectangle!(rect, [0, 0, 255]) if rect.area > 0
 end
+
+output_file = ARGV[1] || 'marked.jpg'
+img.save_jpeg(output_file)
