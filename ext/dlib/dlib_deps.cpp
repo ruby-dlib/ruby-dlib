a3d40600c6b609c6374a26135d4d3db3632e1e4f
Kenta Murata <mrkn@cookpad.com>
2015-03-02 15:19:12 +0900

    Exchange the order to load jpeg and png

--- ext/dlib/dlib_deps.cpp
+++ ext/dlib/dlib_deps.cpp
@@ -1,5 +1,5 @@
-#include <dlib/image_loader/jpeg_loader.cpp>
-#include <dlib/image_saver/save_jpeg.cpp>
-
 #include <dlib/image_loader/png_loader.cpp>
 #include <dlib/image_saver/save_png.cpp>
+
+#include <dlib/image_loader/jpeg_loader.cpp>
+#include <dlib/image_saver/save_jpeg.cpp>
