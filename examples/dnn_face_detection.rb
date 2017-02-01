eb01f41cc5265fd008baab3ae490d6d6b575d10c
ainame <s.namai.09@gmail.com>
2017-02-01 06:24:58 +0000

    Fix example

--- examples/dnn_face_detection.rb
+++ examples/dnn_face_detection.rb
@@ -6,14 +6,26 @@ image_file = ARGV[0] || 'ext/dlib-19.2/examples/faces/2009_004587.jpg'
 model_file = 'mmod_human_face_detector.dat'
 detector = Dlib::DNNFaceDetector.new(model_file)
 
+puts 'single detections'
+
 img = Dlib::Image.load(image_file)
 rects = detector.detect(img)
 
 puts "number of faces detected #{rects.length}"
 rects.each.with_index do |rect, index|
   puts "Detection #{index}: Left: #{rect.left} Top: #{rect.top} Right: #{rect.right} Bottom: #{rect.bottom} Area: #{rect.area}"
-  img.draw_rectangle!(rect, [0, 0, 255]) if rect.area > 0
 end
 
-output_file = ARGV[1] || 'faces.jpg'
-img.save_jpeg(output_file)
+puts ''
+puts 'multiple detections'
+
+image = Dlib::Image.load(image_file)
+images = [image, image, image]
+results = detector.detect(images)
+
+results.each do |rects|
+  puts "number of faces detected #{rects.length}"
+  rects.each.with_index do |rect, index|
+    puts "Detection #{index}: Left: #{rect.left} Top: #{rect.top} Right: #{rect.right} Bottom: #{rect.bottom} Area: #{rect.area}"
+  end
+end
